12c1e46d470f031188904a40f9b830db8315b4f4
nghoang1 <57956036+nghoang1@users.noreply.github.com>
2022-12-05 22:44:08 -0600

    fixed several issues

--- getMask.m
+++ getMask.m
@@ -1,33 +1,37 @@
-function mask = getMask(S, maskType, maskPercent, invert)
+function mask = getMask(S, maskType, maskPercent)
     switch maskType 
         case 1
-            if invert == 0
-                mask = zeros(S);
-                value = 1;
-            else
-                mask = ones(S);
-                value = 0;
-                if maskPercent > .9
-                    maskPercent = .9;
-                end
+            
+            mask = ones(S);
+            value = 0;
+            
+            if maskPercent > .9
+
+                maskPercent = .9;
+
             end
+
             offset = round((min(S)/2) * (1-maskPercent));
+
             for i=1+offset:S(1)-offset
+
                 for j=1+offset:S(2)-offset
+
                     mask(i,j) = value;
+
                 end
             end
         case 2
-            radius = round(min(S)/2 * maskPercent);
-            centerW = S(2)/2;
-            centerH = S(1)/2;
-            [W,H] = meshgrid(1:S(2),1:S(1));
-            if invert == 1
-                mask = ((W-centerW).^2 + (H-centerH).^2) > radius^2;
-            else
-                mask = ((W-centerW).^2 + (H-centerH).^2) < radius^2;
-            end
+
+            rad = round(min(S)/2 * maskPercent);
+            cWidth = S(2)/2;
+            cHeight = S(1)/2;
+            [Width,Height] = meshgrid(1:S(2),1:S(1));
+            mask = ((Width-cWidth).^2 + (Height-cHeight).^2) < rad^2;
+
         otherwise
+
             mask = ones(S);
+
     end
 end
